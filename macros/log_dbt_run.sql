0efc7f2050d9e7972c7f0dcffbe811b215321017
Sagar Marthandan <sagar.marthandan@gmail.com>
2025-12-28 13:28:33 +0530

    added hooks + audit_logs + demo_snapshots

---
+++ macros/log_dbt_run.sql
@@ -0,0 +1,31 @@
+-- Macro to log dbt run details into an audit table
+
+{% macro log_dbt_run() %}
+
+{% set query %}
+  insert into BTC.AUDIT.dbt_audit (
+      invocation_id,
+      run_started_at,
+      dbt_command,
+      target_profile,
+      target_name,
+      target_user,
+      dbt_version
+  )
+  values (
+      '{{ invocation_id }}',
+      '{{ run_started_at }}',
+      '{{ invocation_args_dict["invocation_command"] }}',
+      '{{ target.profile_name }}',
+      '{{ target.name }}',
+      '{{ target.user }}',
+      '{{ dbt_version }}'
+  );
+
+commit;
+
+{% endset %}
+
+{% do run_query(query) %}
+
+{% endmacro %}
--- models/stg/stg_btc.sql
+++ models/stg/stg_btc.sql
@@ -4,12 +4,13 @@
     incremental_strategy = 'merge') }}
 
 select
+    '{{ invocation_id }}' as invocation_id,
     *
 from
     {{ source('btc', 'btc') }}
 
 {% if is_incremental() %}
 
-where BLOCK_TIMESTAMP >= (select max(BLOCK_TIMESTAMP) from {{ this }})
+where BLOCK_TIMESTAMP > (select max(BLOCK_TIMESTAMP) from {{ this }})
 
 {% endif %}
